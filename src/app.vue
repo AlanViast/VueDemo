b60f45350f545fc203c4a826e1540aa56f8024b1
AlanViast <AlanViast@gmail.com>
2015-11-19 15:08:38 +0800

    Over

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <h1>{{msg}}</h1>
+  <comp-a></comp-a>
+</div>
+</template>
+
+<script>
+module.exports = {
+  data: function(){
+    return {
+      msg: 'Hello from vue-loader!'
+    };
+  },
+  components: {
+      'comp-a': require('./components/a.vue')
+  }
+};
+
+</script>
